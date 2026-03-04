52a5117000e774ae835e2cd245f8a9e6c48641cb
valik <valik.0404@mail.ru>
2026-03-04 12:05:36 +0300

    refactor: MainLayout

--- src/widgets/layout/MainLayout.tsx
+++ src/widgets/layout/MainLayout.tsx
@@ -5,7 +5,7 @@ interface MainLayoutProps {
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className="h-screen bg-gray-50">
-      <main className="h-[calc(100vh-72px)] overflow-y-auto custom-scroll pb-4 px-4 pt-4">
+      <main className="h-[100vh] overflow-y-auto custom-scroll pb-4 px-4 pt-4">
         {children}
       </main>
     </div>
